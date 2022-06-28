910a0f05c20003719a3c04f3f41fac91c433d9f8
Hadrien Mens-Pellen <hadrien.menspellen@sundayapp.com>
2022-06-28 16:21:18 +0200

    🎨 log outgoing port values

--- public/index.js
+++ public/index.js
@@ -1,6 +1,9 @@
 import { Elm } from "../src/Main.elm";
 import { nanoid } from "nanoid";
 
+const log = name => toLog => console.log(name, toLog)
+
 const app = Elm.Main.init();
 app.ports.playerIdPort.send(nanoid(6));
-app.ports.votes.subscribe(console.log);
+app.ports.votes.subscribe(log("vote"));
+app.ports.player.subscribe(log("player"));
